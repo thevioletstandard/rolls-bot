5e0d87b38339761df4cc5fa75d00dfbcc3ce2522
thevioletstandard <76827395+thevioletstandard@users.noreply.github.com>
2021-02-02 20:38:24 -0500

    code the bot

---
+++ rolls-bot.py
@@ -0,0 +1,50 @@
+import logging
+import praw
+import re
+import secrets
+import time
+
+def roll_die(roll, modifier=""):
+    try:
+        count, maximum = map(int, roll.split("d"))
+        if count > 4096:
+            raise Exception("Too many rolls")
+        rolls = [secrets.randbelow(maximum) + 1 for _ in range(count)]
+        total = sum(rolls)
+        if modifier:
+            if modifier[0] == "+":
+                total += int(modifier[1:])
+            elif modifier[0] == "-":
+                total -= int(modifier[1:])
+            elif modifier[0] == "*":
+                total *= int(modifier[1:])
+            elif modifier[0] == "/":
+                total /= int(modifier[1:])
+            elif modifier[0] == "%":
+                total %= int(modifier[1:])
+            else:
+                raise Exception("Invalid operation")
+        return roll + modifier + ": " + "**" + str(total) + "**" + "\n\n" + str(rolls) + "\n\n"
+    except Exception as oops:
+        logger.error(str(oops) + roll + modifier)
+        return roll + modifier + ": " + "Error: Unable to parse roll." + "\n\n"
+
+for logger_name in ("praw", "prawcore"): # setup logging
+   logging.getLogger(logger_name).addHandler(logging.StreamHandler())
+logging.basicConfig(filename='application.log', format='%(asctime)s %(levelname)s %(name)s %(message)s', level=logging.DEBUG)
+
+reddit = praw.Reddit("rolls-bot") # settings loaded from praw.ini
+for comment in reddit.inbox.stream():
+    if isinstance(comment, praw.models.Comment):
+        dice, response = re.findall(r'\b(\d+d\d+)([+\-*/%]\d+)?\b', comment.body), "" # roll and optional modifier
+        for die in dice:
+            response += roll_die(die[0], die[1])
+        if len(response) > 8192:
+            response = "Error: Too many rolls at once.\n\n"
+        response += "---\n\nI'm a bot that can roll dice if you mention me! Check out r/RollsBot if you need any help."
+        try:
+            comment.reply(response)
+        except praw.exceptions.RedditAPIException as error:
+            time.sleep(min(int(re.find(r'\b(d+)\b', error)) * 60, 600)) # maximum of 10 minutes
+            comment.reply(response)
+    comment.mark_read()
